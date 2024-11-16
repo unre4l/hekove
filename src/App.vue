d59296aca54f38d2bb10c63620076170e72db575
Jonathan Wischhusen <mail@jnthn.ws>
2024-11-16 21:50:54 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,33 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
-async function asd () {
+const pwm = ref(0);
+
+async function post (key: string, value: string|number) {
   const data = new FormData();
-  data.set('pwm', '12');
+  data.set(key, `${value}`);
   const response = await fetch('/set', { method: 'post', body: data});
-  const asda = await response.text();
-  console.log(asda);
+  const respData = await response.json() as { value: string|number };
+  console.log(respData.value);
+}
+
+async function setPwm(value: number) {
+  await post('dutyCycle', value);
+  pwm.value = value;
 }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <nav>
 
+  </nav>
 
-    <button @click="asd">asd</button>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!1" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <div>
+    {{  pwm  }}
+    <input :value="pwm" @change="(e: Event) => setPwm(e.target.value)" type="range" min="0" max="255">
+  </div>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
